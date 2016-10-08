d9ed5d6852423630d028992569761791b5b1004d
Sergey Pererva <proger.su@gmail.com>
2016-10-08 15:08:14 +0300

    Complete

--- js/app.js
+++ js/app.js
@@ -1,41 +1,99 @@
-(function (document) {
+(function (document, window) {
 	$(document).ready(function () {
 		var app = {
-			steps: ['step-0', 'step-1', 'step-2', 'step-3', 'step-4', 'step-5', 'step-6', 'step-7', 'step-8'],
 			init: function () {
+				this.progressBar.init();
 				this.goToStep.init();
 				$('body').show();
 			},
 			goToStep: {
+				first: null,
+				pager: null,
 				init: function () {
-					var hash = window.location.hash.substring(1);
+					this.first = $('.steps .step').first();
+					this.pager = $('.next-step, .prev-step');
+
+					this.pager.on('click', this.go);
 					
-					if(hash && app.steps.indexOf(hash)) {
-						$('body').removeClass().addClass(hash);
-					}
-					
-					$('.go-to-step').on('click', function(e){
-						e.preventDefault;
-						
-						var url = $(this).attr('href'), idx = url.indexOf('#')
-						var step = idx != -1 ? url.substring(idx+1) : '';
-						
-						if(!step && app.steps.indexOf(step)) {
-							return;
-						}
-						
-						$('body').removeClass().addClass(step);
-						
-					});
+					//Go to step by hash
+					this.goByHash();
 				},
 				go: function () {
-
+					//Move slider
+					var $current = $('.steps .current');
+					var $next = $(this).hasClass('next-step') ? $current.next() : $current.prev();
+					var hash = 'step-' + $next.index();
+					
+					if(!$next.length){
+						return;
+					}
+					
+					app.goToStep.move($next.index());
+					
+					app.progressBar.move($next.index());
+					
+					//Update step class
+					$current.removeClass('current');
+					$next.addClass('current');
+					
+					//Update body class
+					$('body').removeClass().addClass(hash);
+					
+					//Update hash
+					window.location.hash = '#' + hash;
+				},
+				
+				goByHash: function(){
+					var hash = window.location.hash.substring(1);
+					var index = hash.replace(/\D/g,'');
+					
+					if(!index){
+						return;
+					}
+					
+					var target = $('.steps').children().eq(index);
+					
+					if(!target.length){
+						return;
+					}
+					
+					$('body').removeClass().addClass(hash);
+					$('.steps').find('.current').removeClass('current');
+					target.addClass('current');
+					
+					this.move(index);
+					
+					app.progressBar.move(index);
+				},
+				
+				move: function(index){
+					this.first.css({
+						'margin-left': '-' + index + '00%'
+					});
 				}
-
-			}
+				
+			},
+			
+			progressBar: {
+				bar: null,
+				bullet: null,
+				steps: null,
+				
+				init: function(){
+					this.bar = $('.progress-bar');
+					this.bullet = $('.bullet', this.bar);
+					this.steps = $('.steps .step').length;
+				},
+				move: function(index){
+					this.bullet.css({
+						'left': ((100 / this.steps) * index) + '%'
+					});
+					this.bullet.text(index);
+				}
+			},
 
 		};
 
 		app.init();
 	});
-}(document));
+}(document, window));
